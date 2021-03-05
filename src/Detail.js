50ae351b6f00428c721cb9a3774a393262c0627b
talha <talhaayub009@gmail.com>
2021-03-05 10:55:21 +0500

    update

--- src/Detail.js
+++ src/Detail.js
@@ -48,122 +48,121 @@ class Detail extends React.Component {
 
             <div>
 
-                <body className="body">
-                    <Navigation />
 
-                    <div className="row ">
+                <Navigation />
 
-                        <div className="col-lg-12 col-sm-12" style={{justifyContent:"center"}}>
-                            <Carousel autoplay style={{ color: "white",justifyContent:"center"}}>
-                                <div className="carousel-item">
+                <div className="row ">
 
-                                    <img src="../headway-5QgIuuBxKwM-unsplash.jpg" style={contentStyle} alt="Responsive image" />
-                                    <div className="carousel-caption " style={{ color: "white", fontSize: "20px" }}>
+                    <div className="col-lg-12 col-sm-12">
+                        <Carousel autoplay style={{ color: "white" }}>
+                            <div className="carousel-item">
 
-                                        <p>Affiliate marketing is a way for companies to incentivize word of mouth marketing or referrals.</p>
-                                    </div>
+                                <img src="../headway-5QgIuuBxKwM-unsplash.jpg" style={contentStyle} alt="Responsive image" />
+                                <div className="carousel-caption " style={{ color: "white", fontSize: "20px" }}>
+
+                                    <p>Affiliate marketing is a way for companies to incentivize word of mouth marketing or referrals.</p>
                                 </div>
-                                <div className="carousel-item">
+                            </div>
+                            <div className="carousel-item">
 
-                                    <img src="../dylan-gillis-KdeqA3aTnBY-unsplash.jpg" style={contentStyle} alt="Responsive image" />
+                                <img src="../dylan-gillis-KdeqA3aTnBY-unsplash.jpg" style={contentStyle} alt="Responsive image" />
 
-                                    <div className="carousel-caption">
+                                <div className="carousel-caption">
 
-                                        <p>We love the Big Apple!</p>
-                                    </div>
-
-                                </div>
-                                <div className="carousel-item" >
-
-                                    <img src="../kobu-agency-7okkFhxrxNw-unsplash.jpg" style={contentStyle} alt="Responsive image" />
-                                    <div className="carousel-caption " style={{ color: "white" }}>
-
-                                        <p>We love the Big Apple!</p>
-                                    </div>
-                                </div>
-                                <div className="carousel-item">
-
-                                    <img src="../brooke-cagle-g1Kr4Ozfoac-unsplash.jpg" style={contentStyle} alt="Responsive image" />
-                                    <div class="carousel-caption" style={{ color: "white" }}>
-                                        <p>We love the Big Apple!</p>
-                                    </div>
+                                    <p>We love the Big Apple!</p>
                                 </div>
 
-                            </Carousel>
+                            </div>
+                            <div className="carousel-item" >
 
-                        </div>
+                                <img src="../kobu-agency-7okkFhxrxNw-unsplash.jpg" style={contentStyle} alt="Responsive image" />
+                                <div className="carousel-caption " style={{ color: "white" }}>
+
+                                    <p>We love the Big Apple!</p>
+                                </div>
+                            </div>
+                            <div className="carousel-item">
+
+                                <img src="../brooke-cagle-g1Kr4Ozfoac-unsplash.jpg" style={contentStyle} alt="Responsive image" />
+                                <div class="carousel-caption" style={{ color: "white" }}>
+                                    <p>We love the Big Apple!</p>
+                                </div>
+                            </div>
+
+                        </Carousel>
+
                     </div>
-                    <div className="container-fluid " style={{ width: "100%" }}>
-                        <div className="row pt-5">
+                </div>
+                <div className="container-fluid " style={{ width: "100%" }}>
+                    <div className="row pt-5">
 
-                            <div className="text-left col-xs-6 col-sm-4 col-lg-4 offset-lg-1 pt-5" style={{ fontSize: "20px", fontFamily: "lft-etica,sans-serif" }}>
-                                <h1 style={{ color: "#4B0082" }}>Performance Drives Our Purpose</h1>
-                                <p style={{ textAlign: "justify" }}>With over 20 years of experience in affiliate and performance marketing, we know how to connect consumers with brands and products in ways no one else can. We continue to innovate with our award-winning solution that connects advertisers with valuable audiences and helps publishers monetise their content.
-                                Access over 150,000 active publishers worldwide, with a dedicated team of network development specialists who actively identify affiliates and partner opportunities across content, influencer, loyalty and more – specifically for you.
+                        <div className="text-left col-xs-6 col-sm-6 col-lg-4 offset-lg-1 pt-5" style={{ fontSize: "20px", fontFamily: "lft-etica,sans-serif" }}>
+                            <h1 style={{ color: "#4B0082" }}>Performance Drives Our Purpose</h1>
+                            <p style={{ textAlign: "justify" }}>With over 20 years of experience in affiliate and performance marketing, we know how to connect consumers with brands and products in ways no one else can. We continue to innovate with our award-winning solution that connects advertisers with valuable audiences and helps publishers monetise their content.
+                            Access over 150,000 active publishers worldwide, with a dedicated team of network development specialists who actively identify affiliates and partner opportunities across content, influencer, loyalty and more – specifically for you.
                         </p>
-                            </div>
-                            <div className="col-xs-12 col-sm-4 col-lg-3  offset-lg-1">
-                                <img src="../mobile.jpg" width="700px" height="500px" className="img-responsive standard-text-block-image" alt="Responsive image" />
-                            </div>
                         </div>
-
-
-                        <div className="row pt-5">
-                            <div className="col-sm-6 col-lg-3  offset-lg-1" style={{ fontSize: "20px", fontFamily: "lft-etica,sans-serif" }}>
-                                <img src="../aff.jpg" width="600px" height="500px" className="img-responsive standard-text-block-image" alt="Responsive image" />
-
-                            </div>
-                            <div className="col-sm-6 col-xs-12 col-lg-4 offset-sm-1 offset-lg-3 pt-5" style={{ fontSize: "20px", fontFamily: "lft-etica,sans-serif" }}>
-                                <h1 style={{ color: "#4B0082" }}>For Agencies</h1>
-                                <p style={{ textAlign: "justify" }}>You need an affiliate marketing partner with reach and scale. Our team provides unparalleled affiliate marketing support with publisher expansion, campaign optimisation, reporting and more.</p>
-                            </div>
-
-                        </div>
-
-                        <div className="row pt-5">
-
-                            <div className="col-xs-12 col-lg-4  offset-lg-1 pt-5 " style={{ fontSize: "20px", fontFamily: "lft-etica,sans-serif" }}>
-                                <h1 style={{ color: "#4B0082" }}>For Publisher Partners</h1>
-                                <p style={{ textAlign: "justify" }}>Work with the top brands across industries, from fashion to financial services and more. Whether you’re a blogger, influencer, app developer or business, our premier global publisher network has you covered.</p>
-                            </div>
-                            <div className="col-sm-12 col-lg-3  offset-lg-2">
-                                <img src="../sev.jpg" width="500px" height="500px" className="img-responsive standard-text-block-image" alt="Responsive image" />
-
-                            </div>
-                        </div>
-
-                        <div className="row pt-5">
-
-                            <div className="col-xs-12 col-sm-12 col-lg-3  offset-lg-1">
-                                <img src="../affiliate.jpg" width="600px" height="500px" className="img-responsive standard-text-block-image" alt="Responsive image" />
-
-                            </div>
-                            <div className="col-xs-12 col-sm-12 col-lg-4  offset-lg-3 pt-5 " style={{ fontSize: "20px", fontFamily: "lft-etica,sans-serif" }}>
-                                <h1 style={{ color: "#4B0082" }}>For Publisher Partners</h1>
-                                <p>Work with the top brands across industries, from fashion to financial services and more. Whether you’re a blogger, influencer, app developer or business, our premier global publisher network has you covered.</p>
-                            </div>
-
-                        </div>
-                        <div className="row pt-5">
-
-
-                            <div className="col-xs-12 col-sm-12 col-lg-5 offset-lg-1 pt-3 " style={{ fontSize: "20px", fontFamily: "lft-etica,sans-serif" }}>
-                                <h1 style={{ color: "#4B0082" }}>5 steps to becoming an affiliate marketer</h1>
-                                <p>The following five steps are all it takes to become an affiliate marketer.</p>
-                                <p>1.Choose a niche - What are we selling?</p>
-                                <p>2.How to generate and grow income - How to select the right program for your niche and understand how commission works?</p>
-                                <p>3.Get a website or blog - Let’s create a home for your content.</p>
-                                <p>4.Create content - This is often based around reviewing products in your niche through your preferred medium.</p>
-                                <p>5.Get rankings and visitors - To connect with your target audience, you need to get some traffic flowing your way.</p>
-                            </div>
-                            <div className="col-sm-4 col-lg-3 offset-lg-1 mt-5 mb-5 pb-5">
-                                <img src="../kl.jpg" width="500px" height="500px" className="img-wrapper" alt="Responsive image" />
-
-                            </div>
+                        <div className="col-xs-12 col-lg-3  offset-lg-1">
+                            <img src="../mobile.jpg" width="700px" height="500px" className="img-responsive standard-text-block-image" alt="Responsive image" />
                         </div>
                     </div>
-                    <Footer />
-                </body>
+
+
+                    <div className="row pt-5">
+                        <div className="col-sm-12 col-lg-3  offset-lg-1" style={{ fontSize: "20px", fontFamily: "lft-etica,sans-serif" }}>
+                            <img src="../aff.jpg" width="600px" height="500px" className="img-responsive standard-text-block-image" alt="Responsive image" />
+
+                        </div>
+                        <div className="col-sm-12 col-xs-12 col-lg-4 offset-sm-1 offset-lg-3 pt-5" style={{ fontSize: "20px", fontFamily: "lft-etica,sans-serif" }}>
+                            <h1 style={{ color: "#4B0082" }}>For Agencies</h1>
+                            <p style={{ textAlign: "justify" }}>You need an affiliate marketing partner with reach and scale. Our team provides unparalleled affiliate marketing support with publisher expansion, campaign optimisation, reporting and more.</p>
+                        </div>
+
+                    </div>
+
+                    <div className="row pt-5">
+
+                        <div className="col-xs-12 col-lg-4  offset-lg-1 pt-5 " style={{ fontSize: "20px", fontFamily: "lft-etica,sans-serif" }}>
+                            <h1 style={{ color: "#4B0082" }}>For Publisher Partners</h1>
+                            <p style={{ textAlign: "justify" }}>Work with the top brands across industries, from fashion to financial services and more. Whether you’re a blogger, influencer, app developer or business, our premier global publisher network has you covered.</p>
+                        </div>
+                        <div className="col-sm-12 col-lg-3  offset-lg-2">
+                            <img src="../sev.jpg" width="500px" height="500px" className="img-responsive standard-text-block-image" alt="Responsive image" />
+
+                        </div>
+                    </div>
+
+                    <div className="row pt-5">
+
+                        <div className="col-xs-12 col-sm-12 col-lg-3  offset-lg-1">
+                            <img src="../affiliate.jpg" width="600px" height="500px" className="img-responsive standard-text-block-image" alt="Responsive image" />
+
+                        </div>
+                        <div className="col-xs-12 col-sm-12 col-lg-4  offset-lg-3 pt-5 " style={{ fontSize: "20px", fontFamily: "lft-etica,sans-serif" }}>
+                            <h1 style={{ color: "#4B0082" }}>For Publisher Partners</h1>
+                            <p>Work with the top brands across industries, from fashion to financial services and more. Whether you’re a blogger, influencer, app developer or business, our premier global publisher network has you covered.</p>
+                        </div>
+
+                    </div>
+                    <div className="row pt-5">
+
+
+                        <div className="col-xs-12 col-lg-5  offset-lg-1 pt-3 " style={{ fontSize: "20px", fontFamily: "lft-etica,sans-serif" }}>
+                            <h1 style={{ color: "#4B0082" }}>5 steps to becoming an affiliate marketer</h1>
+                            <p>The following five steps are all it takes to become an affiliate marketer.</p>
+                            <p>1.Choose a niche - What are we selling?</p>
+                            <p>2.How to generate and grow income - How to select the right program for your niche and understand how commission works?</p>
+                            <p>3.Get a website or blog - Let’s create a home for your content.</p>
+                            <p>4.Create content - This is often based around reviewing products in your niche through your preferred medium.</p>
+                            <p>5.Get rankings and visitors - To connect with your target audience, you need to get some traffic flowing your way.</p>
+                        </div>
+                        <div className="col-sm-12 col-lg-3 offset-lg-1 mt-5 mb-5 pb-5">
+                            <img src="../kl.jpg" width="500px" height="500px" className="img-wrapper" alt="Responsive image" />
+
+                        </div>
+                    </div>
+                </div>
+                <Footer />
             </div>
 
 
